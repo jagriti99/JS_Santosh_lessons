d7501252ae1cec8509252a15fa3db5488eb77d64
Sharma Jagriti <s2200131@BC-C02J61DSQ6LR-MacBook-Air.local>
2022-09-19 12:51:25 +0300

    new task added

---
+++ Practice_lessons1209_react22s/classes/class4.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+Write the Recipe class such that it logs the name and the calories every time
+a new instance of the class is being created..
+**/
+
+// write class definition
+class Recipe {
+  constructor(recipe, calories) {
+    console.log(recipe, calories);
+  }
+}
+
+// Class usage
+let pasta = new Recipe("Pasta", 600);
+console.log("---");
+let salad = new Recipe("Salad", 400);
